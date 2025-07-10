c97d1864f4e23b41c7965e89ee2085124f225912
Andres Felipe Diaz <andresf.diazm@unac.edu.co>
2025-07-10 18:51:53 -0500

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,7 @@ import Home from './pages/Home'
 import Detail from './pages/Detail'
 
 function App() {
-//   const [characters,setCharacters]= useState([]);
-//   const [query,setQuery] =useState('');
-//   const[message,setMessage] = useState('')
 
-    // useEffect(()=>{
-    //     if(!query) return;
-
-    //     const fetchCharacters = async ()=>{
-    //         setMessage('Cargando...')
-    //         try{
-    //             const res = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}`);
-    //             const data = await res.json()
-
-    //             if(data.results){
-    //                 setCharacters(data.results)
-    //                 setMessage(`Mostrando ${data.results.length} resultado(s)`)
-    //             }else{
-    //                 setCharacters([]);
-    //                 setMessage('No se encontraron personajes')
-    //             }
-    //         }catch(error){
-    //             setMessage('Error al buscar personajes');
-    //             console.error(error)
-    //         }
-
-            
-    //     }
-    //     fetchCharacters()
-    // },[query])
-    
     return(
        <Routes>
         <Route path='/' element={<Home/>}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React,{useState,useEffect} from "react";
+import { SearchInput } from "../components/SearchInput";
+import {CharacterList} from "../components/CharacterList"
+import Swal from "sweetalert2";
 
 export const Home = () => {
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  const [characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState("");
+  const [message, setMessage] = useState("");
 
-export default Home
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchCharacters = async () => {
+      setMessage("Cargando...");
+      try {
+        const res = await fetch(
+          `https://rickandmortyapi.com/api/character/?name=${query}`
+        );
+        const data = await res.json();
+
+        if (data.results) {
+          setCharacters(data.results);
+          setMessage(`Mostrando ${data.results.length} resultado(s)`);
+        } else {
+          setCharacters([]);
+          setMessage("No se encontraron personajes");
+          Swal.fire('Ups!','No se encontraron personajes','warning')
+        }
+      } catch (error) {
+        setMessage("Error al buscar personajes");
+        console.error(error);
+        Swal.fire('Error','Nose pudo conectar con la API','error')
+      }
+    };
+    fetchCharacters();
+  }, [query]);
+
+  return (
+    <div className="app">
+      <h1>Home</h1>
+      <SearchInput onSearch={setQuery}/>
+      <p>{message}</p>
+      <CharacterList characters={characters}/>
+    </div>
+  );
+};
+
+export default Home;
