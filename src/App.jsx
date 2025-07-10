95f11ee64b0fd491838ab4031bd743689de1ae97
Andres Felipe Diaz <andresf.diazm@unac.edu.co>
2025-07-10 18:43:21 -0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,45 @@
-import { useState,useEffect } from 'react'
+
 import './App.css'
-import { SearchInput } from './components/SearchInput';
-import { CharacterList } from './components/CharacterList';
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Detail from './pages/Detail'
 
 function App() {
-  const [characters,setCharacters]= useState([]);
-  const [query,setQuery] =useState('');
-  const[message,setMessage] = useState('')
+//   const [characters,setCharacters]= useState([]);
+//   const [query,setQuery] =useState('');
+//   const[message,setMessage] = useState('')
 
-    useEffect(()=>{
-        if(!query) return;
+    // useEffect(()=>{
+    //     if(!query) return;
 
-        const fetchCharacters = async ()=>{
-            setMessage('Cargando...')
-            try{
-                const res = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}`);
-                const data = await res.json()
+    //     const fetchCharacters = async ()=>{
+    //         setMessage('Cargando...')
+    //         try{
+    //             const res = await fetch(`https://rickandmortyapi.com/api/character/?name=${query}`);
+    //             const data = await res.json()
 
-                if(data.results){
-                    setCharacters(data.results)
-                    setMessage(`Mostrando ${data.results.length} resultado(s)`)
-                }else{
-                    setCharacters([]);
-                    setMessage('No se encontraron personajes')
-                }
-            }catch(error){
-                setMessage('Error al buscar personajes');
-                console.error(error)
-            }
+    //             if(data.results){
+    //                 setCharacters(data.results)
+    //                 setMessage(`Mostrando ${data.results.length} resultado(s)`)
+    //             }else{
+    //                 setCharacters([]);
+    //                 setMessage('No se encontraron personajes')
+    //             }
+    //         }catch(error){
+    //             setMessage('Error al buscar personajes');
+    //             console.error(error)
+    //         }
 
             
-        }
-        fetchCharacters()
-    },[query])
-     console.log('Query', query)
-     console.log('Characters', characters)
+    //     }
+    //     fetchCharacters()
+    // },[query])
+    
     return(
-        <div className='app'>
-            <h1>Buscador de personajes en React</h1>
-            <SearchInput onSearch={setQuery}/>
-            <p>{message}</p>
-            <CharacterList characters={characters}/>
-        </div>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/detalle/:id' element={<Detail/>}/>
+       </Routes>
     )
 
 
